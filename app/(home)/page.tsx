e829fdcb2c68c2d496cf15e12921298809b9819d
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-01-30 23:00:55 -0300

    feat: add receptive message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      
+      <div>
+        <h2 className="text-xl font-bold">Olá, Gabriel</h2>
+        <p className="capitalize text-sm" >{
+          format(new Date(), "EEEE',' d 'de' MMMM", {
+            locale: ptBR,
+          })
+          }</p>
+      </div>
     </div>
   );
 }
