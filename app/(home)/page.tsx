3b34482ba2eed1a7eb44d620c092e3e61db3e708
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-02-04 13:22:43 -0300

    feat: add search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,19 +2,24 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
       
-      <div>
+      <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Gabriel</h2>
         <p className="capitalize text-sm" >{
           format(new Date(), "EEEE',' d 'de' MMMM", {
             locale: ptBR,
           })
           }</p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
